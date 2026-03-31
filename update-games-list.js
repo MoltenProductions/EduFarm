ca6dfff9a4db1846414538cf1599c42b00765dca
MoltenProductions <Molten99@icloud.com>
2026-03-31 16:54:31 -0400

    Update update-games-list.js

--- update-games-list.js
+++ update-games-list.js
@@ -4,10 +4,12 @@ const path = require('path');
 const gamesDir = path.join(__dirname, 'games');
 const outputFile = path.join(gamesDir, 'games-list.json');
 
+// Get all folders in /games
 const folders = fs.readdirSync(gamesDir, { withFileTypes: true })
   .filter(dirent => dirent.isDirectory())
   .map(dirent => dirent.name);
 
+// Build list of games with first image in each folder
 const gameList = folders.map(folder => {
   const files = fs.readdirSync(path.join(gamesDir, folder));
   const thumbFile = files.find(f => /\.(png|jpg|jpeg)$/i.test(f));
@@ -17,5 +19,6 @@ const gameList = folders.map(folder => {
   };
 });
 
+// Save to games-list.json
 fs.writeFileSync(outputFile, JSON.stringify(gameList, null, 2));
 console.log('games-list.json updated!');
