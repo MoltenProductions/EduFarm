7a4e41a675a842fe51a0074451bcfefb55b33bb9
MoltenProductions <Molten99@icloud.com>
2026-03-31 19:26:13 -0400

    Update generate-games-json.js

--- generate-games-json.js
+++ generate-games-json.js
@@ -4,9 +4,10 @@ const path = require('path');
 // Path to your /games folder
 const gamesDir = path.join(__dirname, 'games');
 
-// Output JSON file (in root)
+// Output JSON in repo root
 const outputFile = path.join(__dirname, 'games-list.json');
 
+// Read the /games folder
 fs.readdir(gamesDir, { withFileTypes: true }, (err, files) => {
   if (err) {
     console.error('Error reading games folder:', err);
@@ -18,19 +19,22 @@ fs.readdir(gamesDir, { withFileTypes: true }, (err, files) => {
     .map(folder => {
       const folderPath = path.join(gamesDir, folder.name);
 
-      // Find first thumbnail file: jpg or png
+      // Look for first thumbnail file: jpg or png
       const thumbs = fs.readdirSync(folderPath)
         .filter(file => file.match(/\.(jpg|png)$/i));
 
-      // Prefer files with 'thumbnail' in name
+      // Prefer thumbnail.jpg or thumbnail.png
       const thumbFile = thumbs.find(f => f.toLowerCase().includes('thumbnail')) || thumbs[0] || '';
 
       return {
         folder: folder.name,
-        thumb: thumbFile ? `games/${folder.name}/${thumbFile}` : ''
+        thumb: thumbFile ? `games/${folder.name}/${thumbFile}` : 'default-thumbnail.png'
       };
     });
 
+  console.log('Found', gameFolders.length, 'games.');
+  console.log('Writing to:', outputFile);
+
   fs.writeFileSync(outputFile, JSON.stringify(gameFolders, null, 2));
-  console.log('games-list.json created with', gameFolders.length, 'games.');
+  console.log('games-list.json successfully created in repo root!');
 });
